0e2cd0e5ea3909adf579dea8f5a071ac3b89b7b8
dekelshoot <69307490+dekelshoot@users.noreply.github.com>
2024-09-05 12:10:08 +0000

    first

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Key_value_translator
+
+##setup
+veillez tapez la commende suivante pour installer le traducteur 
+``shell
+pip install deep_translator
+``
---
+++ code.py
@@ -0,0 +1,43 @@
+from deep_translator import GoogleTranslator
+
+file_path = 'messages.php'
+translated_file_path = 'messages_fr.php'
+
+# Function to translate the values from English to French in the PHP key-value pairs
+
+def translate_php_array(content, limit=100):
+    translated_content = []
+    count = 0
+    translator = GoogleTranslator(source='en', target='fr')
+
+    for line in content:
+        if "=>" in line and count < limit:
+            key, value = line.split("=>")
+            value = value.strip().strip(",").strip("'").strip()  # Cleaning the value
+
+            # Translate the value
+            translated_value = translator.translate(value)
+
+            # Escape apostrophes in the translated value
+            print(translated_value)
+            if translated_value is not None:
+              translated_value = translated_value.replace("'", "\\'")
+              # Rebuild the line with the translated value
+              translated_line = f"{key} => '{translated_value}',\n"
+              translated_content.append(translated_line)
+            else:
+              translated_content.append(line)
+              print("ligne non traduite: ",line)
+            count += 1
+        else:
+            translated_content.append(line)
+
+    return translated_content
+
+# Translating the first 100 lines
+translated_lines = translate_php_array(file_content, limit=100)
+
+
+# Saving the manually translated content into a new file
+with open(translated_file_path, 'w', encoding='utf-8') as file:
+    file.writelines(translated_lines)
